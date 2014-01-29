4819e0d0cecd08add05ed7b5556b12945c2a91d6
Andy Park <atpark333@gmail.com>
2014-01-28 21:27:38 -1000

    Update count-a-sequence

--- src/four_clj/p22.clj
+++ src/four_clj/p22.clj
@@ -3,5 +3,5 @@
 ; Write a function which returns the total number of elements in a sequence.
 ; Special restrictions: can't use `count`
 
-(defn count-a-sequence [_sequence]
-  (reduce + (map (constantly 1) _sequence)))
+(defn count-a-sequence [s]
+  (reduce + (map (constantly 1) s)))
