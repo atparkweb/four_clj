c8ad2f0283a196029d3ce75cd2757bffc00aef6a
Andy Park <atpark333@gmail.com>
2014-02-05 11:40:50 -1000

    Add problem 28 scaffolding

---
+++ src/four_clj/p28.clj
@@ -0,0 +1,9 @@
+(ns four-clj.p28)
+
+; Write a function which flattens a sequence.
+
+; RESTRICTIONS: Can't use `flatten`
+
+; TODO: Implement flatten-seq
+(defn flatten-seq [s]
+  (throw (Exception. "Not implemented")))
---
+++ test/four_clj/p28_test.clj
@@ -0,0 +1,6 @@
+(ns four-clj.p28-test
+  (:require [clojure.test :refer :all]
+            [four-clj.p28 :refer :all]))
+
+
+; TODO: Add tests
