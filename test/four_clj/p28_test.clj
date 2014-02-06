6e67678b9f7502f55966e8b1cb6f6a464c21b52d
Andy Park <atpark333@gmail.com>
2014-02-06 09:31:44 -1000

    Added tests for problem 28

--- test/four_clj/p28_test.clj
+++ test/four_clj/p28_test.clj
@@ -3,4 +3,14 @@
             [four-clj.p28 :refer :all]))
 
 
-; TODO: Add tests
+(deftest flatten-seq-1
+  (testing "flatten-seq-1"
+    (is (= (flatten-seq '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))))
+
+(deftest flatten-seq-2
+  (testing "flatten-seq-2"
+    (is (= (flatten-seq ["a" ["b"] "c"]) '("a" "b" "c")))))
+
+(deftest flatten-seq-3
+  (testing "flatten-seq-3"
+    (is (= (flatten-seq '((((:a))))) '(:a)))))
