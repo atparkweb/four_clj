0b9dcd087b583c6d5cad1b6fc971636be77a1748
Andy Park <atpark333@gmail.com>
2014-02-16 10:14:20 -1000

    Update problem 30

--- src/four_clj/p30.clj
+++ src/four_clj/p30.clj
@@ -3,4 +3,4 @@
 ; Write a function which removes consecutive duplicates from a sequence.
 
 (defn compress-seq [coll]
-  (reduce #(if (= (last %1) %2) %1 (conj %1 %2)) []))
+  (reduce #(if (= (last %1) %2) %1 (conj %1 %2)) [] coll))
