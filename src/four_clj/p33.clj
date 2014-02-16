e7399fbf7880865966f0cd4f3af3e4a3cdb20ee5
Andy Park <atpark333@gmail.com>
2014-02-16 10:43:26 -1000

    Add scaffolding for problem 33

---
+++ src/four_clj/p33.clj
@@ -0,0 +1,6 @@
+(ns four-clj.p33)
+
+; Write a function which replicates each element of a sequence a variable number of times.
+
+(defn replicate-seq [coll n]
+  (throw (Exception. "Not implemented")))
---
+++ test/four_clj/p33_test.clj
@@ -0,0 +1,24 @@
+(ns four-clj.p33-test
+  (require [clojure.test :refer :all]
+           [four-clj.p33 :refer :all]))
+
+
+(deftest test-1
+  (testing "replicate-seq-1"
+    (is (= (replicate-seq [1 2 3] 2) '(1 1 2 2 3 3)))))
+
+(deftest test-2
+  (testing "replicate-seq-2"
+    (is (= (replicate-seq [:a :b] 4) '(:a :a :a :a :b :b :b :b)))))
+
+(deftest test-3
+  (testing "replicate-seq-3"
+    (is (= (replicate-seq [4 5 6] 1) '(4 5 6)))))
+
+(deftest test-4
+  (testing "replicate-seq-4"
+    (is (= (replicate-seq [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4])))))
+
+(deftest test-5
+  (testing "replicate-seq-5"
+    (is (= (replicate-seq [44 33] 2) [44 44 33 33]))))
