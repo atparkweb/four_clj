7d285effc6a02149bb590e24d7a60d35452d4c02
Andy Park <atpark333@gmail.com>
2014-02-28 10:06:59 -1000

    Solved problem 41

--- src/four_clj/p41.clj
+++ src/four_clj/p41.clj
@@ -2,5 +2,5 @@
 
 ; Write a function which drops every Nth item from a sequence.
 
-(defn drop-nth [coll]
-  (throw (Exception. "Not implemented")))
+(defn drop-nth [coll n]
+  (mapcat pop (map vec (partition n n [nil] coll))))
