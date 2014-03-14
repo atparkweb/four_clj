6278600bd46df522237186adba17288df772f9ed
Andy Park <atpark333@gmail.com>
2014-03-14 13:00:32 -0700

    Add problem 61 scaffolding

---
+++ src/four_clj/p61.clj
@@ -0,0 +1,7 @@
+(ns four-clj.p61)
+
+; Write a function which takes a vector of keys and a vector of values and constructs a map from them.
+; Special Restrictions: zipmap
+
+(defn map-cons [key-coll v-coll]
+  (throw (.Exception "Not implemented")))
---
+++ test/four_clj/p61_test.clj
@@ -0,0 +1,16 @@
+(ns four-clj.p61-test
+  (:require [clojure.test :refer :all]
+            [four-clj.p61 :refer :all]))
+
+
+(deftest map-cons-1
+  (testing "map-cons-1"
+    (is (= (map-cons [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})))
+
+(deftest map-cons-2
+  (testing "map-cons-2"
+    (is (= (map-cons [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})))
+
+(deftest map-cons-3
+  (testing "map-cons-3"
+    (is (= (map-cons [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})))
