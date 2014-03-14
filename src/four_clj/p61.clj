a1045cc62dc716e67b70fab0511210769d85a4a3
Andy Park <atpark333@gmail.com>
2014-03-14 13:16:23 -0700

    Solve problem 61

--- src/four_clj/p61.clj
+++ src/four_clj/p61.clj
@@ -3,5 +3,5 @@
 ; Write a function which takes a vector of keys and a vector of values and constructs a map from them.
 ; Special Restrictions: zipmap
 
-(defn map-cons [key-coll v-coll]
-  (throw (.Exception "Not implemented")))
+(defn map-cons [k-coll v-coll]
+  (apply hash-map (interleave k-coll v-coll)))
--- test/four_clj/p61_test.clj
+++ test/four_clj/p61_test.clj
@@ -5,12 +5,12 @@
 
 (deftest map-cons-1
   (testing "map-cons-1"
-    (is (= (map-cons [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})))
+    (is (= (map-cons [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3}))))
 
 (deftest map-cons-2
   (testing "map-cons-2"
-    (is (= (map-cons [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})))
+    (is (= (map-cons [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"}))))
 
 (deftest map-cons-3
   (testing "map-cons-3"
-    (is (= (map-cons [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})))
+    (is (= (map-cons [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"}))))
