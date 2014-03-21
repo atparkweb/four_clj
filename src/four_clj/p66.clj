19504581acea257f0dba9222dcefd3bfe45aee23
Andy Park <atpark333@gmail.com>
2014-03-20 21:37:31 -1000

    Solve problem 66

--- src/four_clj/p66.clj
+++ src/four_clj/p66.clj
@@ -4,4 +4,6 @@
 
 (defn greatest-common-divisor [x y]
   (let [_max (max x y) _min (min x y)]
-    (apply max (filter #(= (mod _min %) 0) (range 1 (inc _max))))))
+    (apply max (filter #(= (+ (mod _max %) (mod _min %)) 0) (range 1 (inc _min))))))
+
+; BETTER SOLUTION: #(if (zero? %) %2 (recur (mod %2 %) %))
