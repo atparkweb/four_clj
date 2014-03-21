5bd2f3106a8ee294b2e66b9c08da589dc0b08cda
Andy Park <atpark333@gmail.com>
2014-03-20 19:49:26 -0500

    Update p66

--- src/four_clj/p66.clj
+++ src/four_clj/p66.clj
@@ -3,4 +3,5 @@
 ; Given two integers, write a function which returns the greatest common divisor.
 
 (defn greatest-common-divisor [x y]
-  (throw (.Exception "Not implemented")))
+  (let [_max (max x y) _min (min x y)]
+    (apply max (filter #(= (mod _min %) 0) (range 1 (inc _max))))))
