74e5d6cc1a0f407d3d88ecb3e59d6f4599e0e983
Andy Park <atpark333@gmail.com>
2014-03-22 09:49:24 -1000

    Solved problem 81

--- src/four_clj/p81.clj
+++ src/four_clj/p81.clj
@@ -3,5 +3,7 @@
 ; Write a function which returns the intersection of two sets.
 ; The intersection is the sub-set of items that each set has in common.
 
+; SPECIAL RESTRICTION: intersection
+
 (defn set-intersection [s1 s2]
-  (throws (.Exception "Not implemented")))
+  (set (filter #(contains? s1 %) s2)))
